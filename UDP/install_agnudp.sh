1b4fd55bfba276a176c47fa6f802872837311899
hunmai <sanglungmon2024@gmail.com>
2025-07-21 22:16:16 +0700

    Add files via upload

--- UDP/install_agnudp.sh
+++ UDP/install_agnudp.sh
@@ -645,7 +645,7 @@ perform_install_manager_script() {
     local _symlink_path="/usr/local/bin/agnudp"
     
     echo "Downloading manager script..."
-    curl -o "$_manager_script" "https://raw.githubusercontent.com/hunmai/udp/refs/heads/main/agnudp_manager.sh"
+    curl -o "$_manager_script" "https://hunmai.site/UDP/agnudp_manager.sh"
     chmod +x "$_manager_script"
     
     echo "Creating symbolic link to run the manager script using 'agnudp' command..."
